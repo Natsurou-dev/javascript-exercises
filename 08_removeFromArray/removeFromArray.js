948490f1bdea67b30c87b2683050c725bec1ee82
Natsurou <noahpurdy.dev@gmail.com>
2025-10-08 12:51:41 +0900

    Complete 07-11 JS Exercises on Odin Project

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+function removeFromArray(arr, ...exclusions) {
+    let ans = [];
+    for (let i=0; i<arr.length; i++) {
+        if (!(exclusions.includes(arr[i]))) {
+            ans.push(arr[i]);
+        }
+    }
+    return ans;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fDeg) {
+  cDeg = (fDeg - 32) * 5 / 9;
+  cDeg = cDeg.toFixed(1);
+  return parseFloat(cDeg);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cDeg) {
+  fDeg = cDeg * 9 / 5 + 32;
+  fDeg = fDeg.toFixed(1);
+  return parseFloat(fDeg);
 };
 
 // Do not edit below this line
